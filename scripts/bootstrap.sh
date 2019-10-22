0a82cf9051861ec7e109de69fb5ad50ea2c1df3b
Luís Ferreira <contact@lsferreira.net>
2019-10-22 16:42:51 +0100

    scripts: add script to download tsqllint source
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+VERSION="$(tr -d '\n' < tsqllint.version)"
+FILE="v$VERSION.tar.gz"
+RESOURCES_DIR=".resources/"
+
+# Remove possible old source
+rm -rf tsqllint/
+mkdir -p "$RESOURCES_DIR"
+wget -nc -q -O "$RESOURCES_DIR/$FILE" "https://github.com/tsqllint/tsqllint/archive/$FILE"
+tar -zxvf "$RESOURCES_DIR/$FILE"
+mv "tsqllint-$VERSION" tsqllint
