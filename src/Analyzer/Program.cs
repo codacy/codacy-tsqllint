63895703cdb36f02a71d2231dc5506bb22843d0a
José Miguel  Melo <josemiguelmelo94@gmail.com>
2021-06-09 16:07:41 +0100

    feature: Handle failed to load DLL at runtime
    Fixes error: Could not load file or assembly 'System.Collections.Immutable, Version=1.2.5.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'.

--- src/Analyzer/Program.cs
+++ src/Analyzer/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.Loader;
+using System;
+using System.IO;
 
 namespace Codacy.TSQLLint
 {
@@ -7,6 +11,8 @@ namespace Codacy.TSQLLint
         [ExcludeFromCodeCoverage]
         public static int Main()
         {
+            Program.HandleMissingAssemblies();
+
             using (var analyzer = new CodeAnalyzer())
             {
                 analyzer.Run()
@@ -15,5 +21,19 @@ namespace Codacy.TSQLLint
 
             return 0;
         }
+
+        public static void HandleMissingAssemblies()
+        {
+            // https://developer.samsung.com/tizen/blog/en-us/2020/02/17/assembly-loading-problem-in-tizen-net-applications
+            AppDomain.CurrentDomain.AssemblyResolve += (object s, ResolveEventArgs eventArgs) =>
+            {
+                var appDir = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+                var assemblyName = eventArgs.Name.Split(',')[0];
+                var assemblyPath = Path.Combine(appDir, assemblyName + ".dll");
+                return File.Exists(assemblyPath) ? Assembly.LoadFile(assemblyPath) : null;
+            };
+
+        }
     }
+
 }
